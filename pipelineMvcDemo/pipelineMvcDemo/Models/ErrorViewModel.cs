54df0a68cea5067ea5b977ba94b1002577aeabe5
kenendo12 <kenendo12@gmail.com>
2020-05-14 14:37:35 -0500

    ex

---
+++ pipelineMvcDemo/pipelineMvcDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pipelineMvcDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
